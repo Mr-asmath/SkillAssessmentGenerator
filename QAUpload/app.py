c9eefe19fd1ce6b46db8794d1064863fc5f6ef3a
Mohamed Asmathulla <167499696+Mr-asmath@users.noreply.github.com>
2025-12-20 11:26:59 +0530

    Update app.py

--- QAUpload/app.py
+++ QAUpload/app.py
@@ -3245,7 +3245,7 @@ def load_css():
     .level-beginner { background: var(--beginner); color: white; }
     .level-novice { background: var(--novice); color: white; }
     
-    div.stButto{
+    div.stButton{
      background-color:transparent;
     }
     
@@ -3403,6 +3403,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
